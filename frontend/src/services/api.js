ec28bbe6ab8f2499dc521832c86a697665da50a1
Gustavo Isturiz <isturizgustavo@gmail.com>
2024-10-19 18:03:31 +0200

    changes to api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-//const API_BASE_URL = 'https://readwise-8dc6a7dcfdc3.herokuapp.com/api'; // Update this if your backend is hosted elsewhere
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://readwise-8dc6a7dcfdc3.herokuapp.com/api'; // Update this if your backend is hosted elsewhere
 
 export const chatWithAssistant = async (message) => {
   const response = await axios.post(`${API_BASE_URL}/chat`, { message });
